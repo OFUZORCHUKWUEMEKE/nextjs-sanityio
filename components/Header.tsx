8da2c498ac17957b763e7e6b055a62aa019a0f8e
Ofuzor chukwuemeke <uniben2018@gmail.com>
2022-07-14 12:49:21 +0100

    CI/CD

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,7 +7,8 @@ const Header = () => {
     <header className='flex justify-between p-5 max-w-7xl mx-auto '>
         <div className='flex items-center space-x-5'>
             <Link href='/'>
-                <img  className='w-44 object-contain cursor-pointer ' src="https://links.papareact.com/yvf" alt=""/>
+                {/* <img  className='w-44 object-contain cursor-pointer ' src="https://links.papareact.com/yvf" alt=""/> */}
+                <h1 className="text-3xl font-bold font-mono">Blugg</h1>
             </Link>
             <div className='hidden md:inline-flex items-center space-x-5'>
                 <h3>About</h3>
